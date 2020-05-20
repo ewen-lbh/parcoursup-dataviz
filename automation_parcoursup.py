97b322e1f102aecf4f8b1b56355d34679674f657
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2020-05-20 11:21:19 +0200

    ✨ List of wishes w/ name, rank, diffs

--- automation_parcoursup.py
+++ automation_parcoursup.py
@@ -3,35 +3,76 @@ from dotenv import load_dotenv
 from os import path, getenv
 from time import sleep
 import re
-load_dotenv(path.join(path.dirname(__file__), '.parcoursup_creds'))
+from os import makedirs
+from bs4 import BeautifulSoup
+from pastel import colorize
+from docopt import docopt
 
-browser = start_firefox('https://dossierappel.parcoursup.fr/Candidat/authentification')
+def run():
+    load_dotenv(path.expanduser('~/.parcoursup_creds'))
 
-try:
-    # Login
-    write(getenv('PARCOURSUP_ID'), into='N° de dossier')
-    write(getenv('PARCOURSUP_PASS'), into='Mot de passe')
-    click('Connexion')
+    args = docopt("""Usage: automation_parcoursup.py [options]
 
-    # Récup l'HTML
-    sleep(10)
-    raw_html = S('body').web_element.get_attribute('innerHTML')
-    
-    # wishes = find_all(S('#voeux_enattente tr'))
-    # print(wishes)
-    # # Pour chaque vœux
-    # for wish in wishes:
-    #     continue
-    #     # Récup l'id
-    #     wish_id = wish.web_element.get_attribute('id').replace('-', '_')
-    #     # Récup le nom
-    #     print(dir(wish.web_element))
-    #     wish_name = wish.web_element.find_element('td:nth-child(3)')
-    #     print(f'Vœu: {wish_name}')
-    #     # Récup la popup
-    #     ranks_sel = f'#lst_att_{wish_id} #rang_cddt '
-    #     position = S(ranks_sel + '> ul:first-child > li:first-child > span.strong').web_element.text
+    --cache  Use the cache (in ~/.cache/automation-parcoursup/admission.html)
+    """)
+
+    # Récup le cache si présent
+    cachedir = path.expanduser('~/.cache/automation-parcoursup')
+    if not path.exists(cachedir):
+        makedirs(cachedir)
+    cachefile = path.join(cachedir, 'admission.html')
+    if path.exists(cachefile) and args['--cache']:
+        soup = BeautifulSoup(open(cachefile).read(), features='lxml')
+    else:
+        browser = start_firefox('https://dossierappel.parcoursup.fr/Candidat/authentification')
+        # Login
+        write(getenv('PARCOURSUP_ID'), into='N° de dossier')
+        write(getenv('PARCOURSUP_PASS'), into='Mot de passe')
+        click('Connexion')
+        # On attend le chargement de la page
+        sleep(3)
+        # On récup l'HTML de la page
+        raw_html = S('body').web_element.get_attribute('innerHTML')
+        # Fermer le navigateur, on en a plus besoin
+        browser.close()
+        # On écrit le cache
+        with open(cachefile, 'w') as file:
+            file.write(raw_html)
+        print('Written response to cache.')
+        # On parse l'HTML avec bs4
+        soup = BeautifulSoup(raw_html, features='lxml')
+
+
+    # On récupère les vœux
+    wishes = soup.find(id='voeux_enattente').find_all('tr', class_='voeu')
+
+    # Pour chaque vœux
+    for wish in wishes:
+        # Récup l'id
+        wish_id = wish['id'].replace('-', '_')
+        # Récup le nom
+        cells = wish.find_all('td')
+        wish_name = cells[2].string.strip().replace('\n', ' ').replace('\t', ' ')
+        # Enlever les espaces dupliqués
+        wish_name = re.sub(r' {2,}', ' ', wish_name)
+        # Récup les rangs dans la popup
+        popup = soup.find('div', id=f'lst_att_{wish_id}').find(id='rang_cddt')
+        try:
+            rank = int(popup.find_all('ul')[0].find_all('li')[0].find('span', class_='strong').string)
+            last_year_max_rank = int(popup.find_all('ul')[1].find_all('li')[2].find('span', class_='strong').string)
         
-    
-finally:
-    browser.close()
+        except IndexError:
+            print(colorize(f'<options=bold>Vœu:</> <options=dark>{wish_name}</>'))
+            print('\t(Internat)')
+        else:
+            diff_abs = last_year_max_rank - rank
+            diff_rel = diff_abs / last_year_max_rank
+            color = 'green' if diff_abs >= 0 else 'red'
+            print(colorize(f'<options=bold;fg={color}>Vœu:</> <options=dark>{wish_name}</>'))
+            print(colorize(f'\tYours         <options=dark>#</><options=bold>{rank}</>'))
+            print(colorize(f'\tMax last year <options=dark>#</><options=bold>{last_year_max_rank}</>'))
+            print(colorize(f'\tDelta:      <options=dark>rel: </><options=bold>{diff_rel*100:.1f}</><options=dark>%</>'))
+            print(colorize(f'\t            <options=dark>abs: </><options=bold>{diff_abs}</>'))
+        print('')
+        # ranks_sel = f'#lst_att_{wish_id} #rang_cddt '
+        # position = S(ranks_sel + '> ul:first-child > li:first-child > span.strong').web_element.text
